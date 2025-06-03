f62d4919d3559c9947f074b3460daabf7c0c9ba4
Darrzej <kastratidarsej@gmail.com>
2025-06-03 14:16:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+# num1 = float(input("Enter first number: "))
+# op = input("Enter operator (+, -, *, /): ")
+# num2 = float(input("Enter second number: "))
+
+# if op == "+":
+#     print(num1 + num2)
+# elif op == "-":
+#     print(num1 - num2)
+# elif op == "*":
+#     print(num1 * num2)
+# elif op == "/":
+#     print(num1 / num2)
+# else:
+#     print("Invalid operator")
+# Simple calculator_______
+
+
+# import random
+
+# number = random.randint(1, 10)
+# guess = int(input("Guess a number (1–10): "))
+# if guess == number:
+#     print("Correct! 🎉")
+# else:
+#     print(f"Wrong! It was {number}.")
+# Guess the number___________
+
+
+# text = input("Enter a word: ")
+# if text == text[::-1]:
+#     print("Palindrome!")
+# else:
+#     print("Not a palindrome.")
+
+
+# for i in range(1, 6):
+#     print("*" * i)
+
+
+
